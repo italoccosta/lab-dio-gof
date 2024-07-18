25ccc2a4af87c8cf24f0d5d63605bd4bdf68c6d2
italo_costa <italo.costa13@hotmail.com>
2024-07-18 17:08:55 -0300

    primeiro commit! Criando projeto com Spring initializr
    Signed-off-by: italo_costa <italo.costa13@hotmail.com>

---
+++ src/main/java/lab/dio/gof/LabDioGofApplication.java
@@ -0,0 +1,13 @@
+package lab.dio.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDioGofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDioGofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/dio/gof/LabDioGofApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.dio.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDioGofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
